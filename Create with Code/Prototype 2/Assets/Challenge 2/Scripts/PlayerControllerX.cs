79dde6a949bb8c628ca51cd7d248c34e7022d0ca
Caroline Percy <lineypercy@me.com>
2022-03-26 20:08:02 +0000

    Completed Challenge 2

---
+++ Create with Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float currentTime = 0.0f;
+    private float timeBetweenDogs = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > 0)
+            currentTime -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && currentTime <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            currentTime = timeBetweenDogs;
+        }
+    }
+}
