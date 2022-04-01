6abb901674f9dc680ab2be36dd250e624598f259
Caroline Percy <lineypercy@me.com>
2022-04-01 17:40:34 +0100

    Endless waves of Enemies and Powerups

--- Create with Code/Prototype 4/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     private Rigidbody playerRb;
+
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
+
+    public bool hasPowerup;
+    float powerupStrength = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +25,39 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+
+            powerupIndicator.gameObject.SetActive(true);
+
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + "with powerup set to " + hasPowerup);
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        powerupIndicator.gameObject.SetActive(false);
+        hasPowerup = false;
     }
 }
